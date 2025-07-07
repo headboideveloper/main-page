b53530bb68d7137721f36395a8be55c57715b8c2
headboideveloper <godofman3@gmail.com>
2025-07-07 22:04:40 +0100

    second update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,24 @@ import logo from '../../space/logo.png'
 
 const Footer = () => {
   return (
-    <div className='h-[150px] md:mt-[-80px] max-w-screen bg-gray-100 mt-[640px] py-8 text-center text-gray-400 md:py-10'>
-      <div className='flex flex-col space-y-4 space-x-0 items-center justify-evenly md:flex-row md:space-y-0 md:space-x-4 md:py-4'>
-      <a href="#navbar">
-        <img src={logo} alt="" className='w-[240px] md:w-[300px] px-4'/>
-      </a>
+//     <div className='h-[150px] md:mt-[-80px] max-w-screen bg-gray-100 mt-[640px] py-12 text-center text-gray-400 md:py-10'>
+//       <div className='flex flex-col space-y-4 space-x-0 items-center justify-evenly md:flex-row md:space-y-0 md:space-x-4 md:py-4'>
+//       <a href="#navbar">
+//         <img src={logo} alt="" className='w-[240px] md:w-[300px] px-4'/>
+//       </a>
+//       <p className='text-gray-400 py-2'> © 2025 Syntax Synergy. All rights reserved.</p>
+//       </div>
+// </div>
+
+          <div className='max-w-screen px-4 bg-white mt-[750px] md:mt-[-80px] py-10 text-gray-400'>
+            <div className='flex flex-col item-center space-y-4 space-x-0 md:flex-row md:space-x-4 md:space-y-0 text-center items-center justify-evenly'>
+            <a href="#navbar">
+       <img src={logo} alt="" className='w-[240px] md:w-[300px] px-4'/>
+            </a>
       <p className='text-gray-400 py-2'> © 2025 Syntax Synergy. All rights reserved.</p>
-      </div>
-</div>
+     </div>
+            </div>
+          
   )
 }
 
